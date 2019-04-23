47a65b945fb6bcb0e61f85d5d6cd2538f6fac9f5
U. Mariano Gálvez <alumno@labs.academico>
2019-04-23 11:56:31 -0600

    primera version push pop vaciar y eliminar

---
+++ Pilas/src/pilas/Lista.java
@@ -0,0 +1,99 @@
+
+package pilas;
+
+public class Lista {
+    // puntero de inicio o principal
+    	private Nodo cabeza=null;
+        
+        // variable de longitud de la lista
+	private int longitud=0;
+        
+        
+	private class Nodo {
+		public Libro libro; // estructura del libro
+		public Nodo siguiente=null; // puntero para enlazar
+                
+                //Constructor para insertar al final
+		public Nodo(Libro libro) {
+			this.libro=libro;
+		}
+        }
+
+// apilar
+public void push(Libro libro){
+
+    Nodo nodo=new Nodo(libro); // crea un nuevo nodo Tipo Libro
+       nodo.siguiente=cabeza; // nodo siguiente ahora apunta a nulo
+		cabeza=nodo;
+		longitud++;	
+}
+
+// desapilar
+public void pop (){
+       if(cabeza == cabeza.siguiente){
+                cabeza = null;
+                cabeza.siguiente = null;
+            }else{
+                cabeza = cabeza.siguiente;
+            }
+}
+
+public void vaciarPila()
+{
+                if (cabeza!=null){
+                Nodo Primer = cabeza;
+                cabeza=cabeza.siguiente=null;
+                Primer.siguiente = null;
+                cabeza = cabeza;
+                longitud--;
+   }
+  }
+
+public  int  tamaño(){
+    return longitud;
+}
+
+ public boolean estaVacia() {
+            return cabeza==null;
+        }
+
+ public Libro obtenerPila(){
+     if (cabeza== null){
+         return null;
+     }else{
+         return cabeza.libro;
+     }
+ }
+ 
+ public Libro obtener( int n ) {
+                
+		if (cabeza==null) {
+			return null;
+		} else {
+			Nodo puntero=cabeza;
+			int contador=0;
+			while (contador<n && puntero.siguiente!=null) {
+				puntero=puntero.siguiente;
+				contador++;
+			}
+			if (contador!=n) {
+				return null;
+			} else {
+				return puntero.libro;
+			}
+		}
+	}
+ 
+ 
+   public String Listar()
+	{
+		String Dato=" "; // crea una varibable que guarda los datos asignados a la lista
+		Nodo aux=cabeza; // nuevo nodo que recorre la lista
+		while (aux!=null) // muestra todos los datos mientras que no sea nulo
+		{
+		/*Datos que se van a mostrar*/Dato+="" + aux.libro.getAutor()+"\n" + aux.libro.getTitulo()+"\n" +aux.libro.getIsbn()+"\n\n" + "";
+		/*avanza al siguiente*/aux=aux.siguiente; // 
+		}
+		return(Dato); 
+	}
+}
---
+++ Pilas/src/pilas/Pilas.java
@@ -0,0 +1,75 @@
+
+package pilas;
+
+import javax.swing.JOptionPane;
+
+
+public class Pilas {
+
+        
+   static  Libro libro = new Libro(""," ",""); // estructura libro que recibe los 3 parametros de los datos a utilizar
+     private static String autores; // variable que almacena los autores
+      public static String titulos; // variable que almacena el titulo
+      public static String isbn; // variable que almacena e; isbn de los libros
+      static   Lista fun = new Lista (); // Llama a l metodo lista donde se encuentras los metodos para realizar con el nodo
+      static int iInsertarPos; // variable que inserta la posicion donde de deseo insertar datos
+      static int iBorrarLibro; // varable que me permiete borrar el dato deseado
+     
+   
+    public static void main(String[] args) {
+     int iOpc = 0; // variable de la opciones que se utilizaran en el menu
+        
+     // Menu
+     do{
+            
+            iOpc = Integer.parseInt(JOptionPane.showInputDialog("Bienvenido que operacion desea realizar:\n1.push\n2.Mostrar\n3.pop\n4.vaciar toda la pila\n5.obtener\n6.Salir"));
+             
+             switch(iOpc){
+                 // inserta datos al principio
+                  case 1: 
+                      insertarDato();
+                      libro = new Libro(titulos,autores,isbn);
+                      fun.push(libro);
+                                        break;   
+                                        
+                  // muestra los datos
+                  case 2:
+                   JOptionPane.showMessageDialog(null,"Datos\n"+ fun.Listar()); 
+                      break;
+                   
+                      // desapila los datos 
+                  case 3:
+                      libro = new Libro(titulos,autores,isbn);
+                      fun.pop();
+                      break;
+                      
+                      // vacia la pila 
+                  case 4:
+                      libro = new Libro(titulos,autores,isbn);
+                      fun.vaciarPila();
+                      break;
+                      
+                      // obtener la pila
+                  case 5:
+                      libro = new Libro(titulos,autores,isbn);
+                      fun.obtenerPila();
+                      break;
+                      
+        
+             
+             }
+      
+    }while(iOpc !=6);
+       
+   
+        }
+    
+public static void  insertarDato(  ){
+           titulos = JOptionPane.showInputDialog("Ingrese el titulo del libro");
+          autores =   JOptionPane.showInputDialog("Ingrese el autor");
+          isbn =   JOptionPane.showInputDialog("Ingrese el isbn");
+                        
+              
+   }
+    
+}
